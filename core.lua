4e633ba75218cecb7a64fc0e7b1c41414e55fa24
Rick Warrington <phial1020@gmail.com>
2015-03-03 19:48:58 -0700

    Initial commit

---
+++ core.lua
@@ -0,0 +1,86 @@
+-- create AddOn frame 
+local selfieWatch = CreateFrame("FRAME", "selfieFrame", UIParent)
+local g_IsInCombat = false;
+local g_Selfies = {};
+
+-- decides whether or not a selfie should be tracked
+local function SelfieWatch_OnSelfieTaken(sourceGUID)
+    -- if not in a combat state then don't track selfies
+    if(not g_IsInCombat) then
+        return;
+    end
+
+    local className, classId, raceName, raceId, gender, name, realm = GetPlayerInfoByGUID(sourceGUID);
+
+    -- if the unit taking the selfie is not the player then check if they are
+    -- in the party or raid.
+    if(name ~= select(1, UnitName("player"))) then
+        if(not UnitInParty(sourceGUID) or not UnitInRaid(sourceGUID)) then
+            return;
+        end
+    end
+
+    -- at this point the player or someone in the raid or party has taken a
+    -- selfie in combat
+    print("SELFIE ALERT: " , name , " (" , raceName , ", " , className , ")");
+    SelfieWatch_LogSelfie(unitName);
+end
+
+-- logs the in-combat selfie event
+local function SelfieWatch_LogSelfie(unitName)
+    if(g_Selfies[unitName] == nil) then
+        g_Selfies[unitName] = { count: 0 };
+    end
+
+    g_Selfies[unitName].count = 1;
+end
+
+-- write the selfies taken to the output stream
+local function SelfieWatch_AnnounceSelfiesTaken()
+end
+
+-- handles events registered by the addon
+local function SelfieWatch_OnEvent(self, event, ...)
+    if(g_IsInCombat ~= InCombatLockdown()) then
+        g_IsInCombat = InCombatLockdown();
+        print("Switching combat state to ", g_IsInCombat);
+        if(g_IsInCombat) then
+            print("Create a new selfie tracking object");
+            g_Selfies = {};
+        else
+            print("Can print or do w/e here idc");
+            SelfieWatch_AnnounceSelfiesTaken();
+        end
+    end
+
+    if(event == "ADDON_LOADED" or event == "PLAYER_LOGIN") then
+        print("Logged in");
+    end
+
+    if(event == "COMBAT_LOG_EVENT_UNFILTERED") then
+        local timeStamp, eventType, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, spellID, spellName, spellSchool, amount, overkill = ...;
+        if(eventType == "SPELL_CAST_SUCCESS") then
+            if(spellID == 181842 or spellName == "Take Selfie") then
+                SelfieWatch_OnSelfieTaken(sourceGUID);
+            end
+        end
+    end
+end
+
+-- handles slash commands
+local function SelfieWatch_CommandHandler(command)
+    print("Selfie command: ", command);
+end
+
+-- register events
+selfieWatch:RegisterEvent("ADDON_LOADED");
+selfieWatch:RegisterEvent("PLAYER_LOGIN");
+selfieWatch:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+selfieWatch:RegisterEvent("PLAYER_REGEN_ENABLED");
+selfieWatch:RegisterEvent("PLAYER_REGEN_DISABLED");
+selfieWatch:SetScript("OnEvent", SelfieWatch_OnEvent);
+
+-- slash commands
+SLASH_SELFIEWATCH1 = "/sw";
+SlashCmdList["SELFIEWATCH"] =  SelfieWatch_CommandHandler;
+
